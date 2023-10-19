7ddc55e69a5f81f225929fc5bf6af1d157c70d5b
polar-bet <pbear8190@gmail.com>
2023-10-19 03:22:49 +0300

    created Stats class with methods

--- src/main/java/org/example/Stats.java
+++ src/main/java/org/example/Stats.java
@@ -6,44 +6,105 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Optional;
 
 
 public class Stats {
 
+    private final static String STATS_FILE_PATH = "./src/main/resources/user-info.json";
+
     public static void printStats() {
-        JSONParser parser = new JSONParser();
+        JSONArray players = getStatsJSON();
         try {
-            InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("user-info.json");
-
-            InputStreamReader reader = new InputStreamReader(inputStream);
-
-            Object obj = parser.parse(reader);
-
-            JSONObject jsonObject = (JSONObject) obj;
-            JSONArray players = (JSONArray) jsonObject.get("players");
-
             System.out.printf(Game.ANSI_BLUE + "-------------------------%n");
             System.out.printf("| %-10s | %-8s |%n", "ІМ'Я", "ОЧКИ");
             System.out.printf("-------------------------%n");
-            for (Object player : players) {
-                JSONObject objPlayer = (JSONObject) player;
-                String name = (String) objPlayer.get("name");
-                Long score = (Long) objPlayer.get("score");
+            for (Object objPlayer : players) {
+                JSONObject player = (JSONObject) objPlayer;
+                String name = (String) player.get("name");
+                Long score = (Long) player.get("score");
                 System.out.printf("| %-10s | %-8s |%n", name, score);
                 if (players.indexOf(player) == players.size() - 1) {
                     System.out.printf("-------------------------%n" + Game.ANSI_RESET);
                 }
             }
-            inputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
+    public static JSONArray getStatsJSON() {
+        JSONParser parser = new JSONParser();
+        JSONArray players = new JSONArray();
+        try {
+            FileReader reader = new FileReader(STATS_FILE_PATH);
+
+            if (isFileEmpty()){
+                return new JSONArray();
+            }
+
+            Object obj = parser.parse(reader);
+
+            reader.close();
+
+            JSONObject jsonObject = (JSONObject) obj;
+            players = (JSONArray) jsonObject.get("players");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return players;
+    }
+
+    public static boolean isFileEmpty() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(STATS_FILE_PATH))) {
+            return reader.readLine() == null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false; // Handle the exception appropriately in your code
+        }
+    }
+
+    public static void addStats(String name, long score) {
+
+        boolean isPlayerExists = false;
+        JSONArray players = getStatsJSON(); // Initialize an empty array
+        try {
+            for (Object playerObj : players) {
+                JSONObject player = (JSONObject) playerObj;
+                if (player.get("name").equals(name)) {
+                    isPlayerExists = true;
+                    long currentScore = Long.parseLong(player.get("score").toString());
+                    player.replace("score", currentScore + score);
+                }
+            }
+
+            if (!isPlayerExists) {
+                JSONObject player = new JSONObject();
+                player.put("name", name);
+                player.put("score", score);
+                players.add(player);
+                System.out.println(player);
+            }
+
+            writeToFile(players);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToFile(JSONArray players) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("players", players);
+
+            FileWriter fileWriter = new FileWriter(STATS_FILE_PATH);
+            fileWriter.write(jsonObject.toJSONString());
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
